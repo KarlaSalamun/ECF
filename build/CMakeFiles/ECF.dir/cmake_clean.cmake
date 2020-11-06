fdbeacbd25a73d73fa8f1c840d1196935f2df443
Karla <karlasalamun9@gmail.com>
2020-11-06 11:44:05 +0100

    added ECF fork

---
+++ build/CMakeFiles/ECF.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ECF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/GPArtificialAnt/build/CMakeFiles/GPArtificialAnt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GPArtificialAnt.dir/AntEvalOp.cpp.o"
+  "CMakeFiles/GPArtificialAnt.dir/main.cpp.o"
+  "GPArtificialAnt.pdb"
+  "GPArtificialAnt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GPArtificialAnt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
